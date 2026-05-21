eafff9f4ccc9571a543889438325f4764a55c487
goblock1938 <goblock1938@gmail.com>
2026-05-21 18:31:17 +0700

    insel minggu 12

--- Linked List/DLL.c
+++ Linked List/DLL.c
@@ -3,11 +3,11 @@
 
 typedef struct node {
   int data;
-  struct node *next;
-  struct node *prev;
+  struct node *next, *prev;
 } Node;
 
-Node simpul, *new, *search, *head, *tail;
+Node simpul, *new = NULL, *search = NULL, *ps = NULL, *head = NULL,
+             *tail = NULL;
 
 void in();
 void del();
@@ -19,11 +19,12 @@ void inbef();
 void delaw();
 void delak();
 void delq();
+void tampil();
 
 int main() {
   int choice;
   do {
-    printf("\nMENU SLL : \n");
+    printf("\nMENU DLL : \n");
     printf("1. Insert\n");
     printf("2. Delete\n");
     printf("3. Exit\n");
@@ -43,6 +44,7 @@ int main() {
       printf("INVALID CHOICE!!!\n");
       break;
     }
+    tampil();
   } while (choice != 3);
 }
 
@@ -52,12 +54,12 @@ void alokasi() {
     printf("ALOCATION FAILED!!!\n");
     return;
   }
-  printf("Alokasi berhasil\n");
+  printf("Alokasi berhasil\n\n");
   int nilai;
   printf("masukkan data : ");
   scanf("%d", &nilai);
   new->data = nilai;
-  new->next = new->prev = NULL;
+  new->next = NULL;
 }
 
 void in() {
@@ -91,7 +93,7 @@ void in() {
       printf("INVALID CHOICE!!!\n");
       break;
     }
-  } while (choice != 5);
+  } while (choice < 1 || choice > 5);
 }
 
 void del() {
@@ -107,13 +109,13 @@ void del() {
 
     switch (choice) {
     case 1:
-      inaw();
+      delaw();
       break;
     case 2:
-      inak();
+      delak();
       break;
     case 3:
-      inaf();
+      delq();
       break;
     case 4:
       break;
@@ -121,5 +123,160 @@ void del() {
       printf("INVALID CHOICE!!!\n");
       break;
     }
-  } while (choice != 4);
+  } while (choice > 4 || choice < 1);
+}
+
+void inaw() {
+  alokasi();
+
+  if (!new)
+    return;
+
+  if (head == NULL) {
+    head = new;
+    tail = new;
+  } else {
+    new->next = head;
+    head = new;
+  }
+}
+
+void inak() {
+  alokasi();
+
+  if (!new)
+    return;
+
+  if (head == NULL) {
+    head = new;
+    tail = new;
+  } else {
+    tail->next = new;
+    tail = new;
+  }
+}
+
+void inaf() {
+  int key;
+  printf("Masukkan nilai key : ");
+  scanf("%d", &key);
+  search = head;
+
+  while (search != NULL && search->data != key)
+    search = search->next;
+
+  if (search == NULL) {
+    printf("KEY NOT FOUND !!!\n");
+    return;
+  }
+
+  if (tail->data == key) {
+    inak();
+    return;
+  }
+
+  alokasi();
+  if (!new)
+    return;
+  new->next = search->next;
+  search->next = new;
+}
+
+void inbef() {
+  int key;
+  printf("Masukkan nilai key : ");
+  scanf("%d", &key);
+  search = head;
+
+  while (search != NULL && search->data != key) {
+    ps = search;
+    search = search->next;
+  }
+  if (search == NULL) {
+    printf("KEY NOT FOUND !!!\n");
+    return;
+  }
+
+  alokasi();
+  if (!new)
+    return;
+
+  new->next = search;
+  ps->next = new;
+}
+
+void delaw() {
+  if (head == NULL) {
+    printf("SLL kosong!\n");
+    return;
+  }
+
+  search = head;
+  head = head->next;
+  free(search);
+  search = NULL;
+}
+
+void delak() {
+  if (head == NULL) {
+    printf("SLL kosong!\n");
+    return;
+  }
+
+  search = tail;
+  ps = head;
+
+  if (head == tail) {
+    free(head);
+    head = tail = NULL;
+    return;
+  }
+
+  while (ps->next != tail)
+    ps = ps->next;
+
+  tail = ps;
+  tail->next = NULL;
+  free(search);
+  search = NULL;
+  ps = NULL;
+}
+void delq() {
+  int key;
+  printf("Masukkan key : ");
+  scanf("%d", &key);
+
+  if (head->data == key)
+    delaw();
+
+  else if (tail->data == key)
+    delak();
+
+  else {
+    search = head;
+
+    while (search != NULL && search->data != key) {
+      ps = search;
+      search = search->next;
+    }
+
+    if (search == NULL) {
+      printf("KEY NOT FOUND !!!\n");
+      return;
+    }
+
+    ps->next = search->next;
+    free(search);
+    search = NULL;
+  }
+}
+
+void tampil() {
+  search = head;
+  printf("\nHEAD");
+  while (search != NULL) {
+    printf("->%d", search->data);
+    search = search->next;
+  }
+  printf("<-TAIL\n");
 }
