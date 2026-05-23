29555597b7ab005e1bca7a9ddae5a4fab6e557f5
goblock1938 <goblock1938@gmail.com>
2026-05-23 15:05:03 +0700

    insel minggu 12

--- sorting/insel-2.c
+++ sorting/insel-2.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
-#define MAKS 10
+#include <string.h>
+#define MAKS 20
 
-void insertion(int[], int);
-void selection(int[], int);
-void tampil(int[]);
-void tukar(int *, int *);
+typedef struct data {
+  int no;
+  char nama[MAKS];
+  char nilai;
+} Data;
+
+void insertion(Data[], int, int);
+void selection(Data[], int, int);
+void tampil(Data[], int);
+void tukar(Data *, Data *);
+int input(Data[]);
+void copy(Data *, Data *);
 
 int main() {
   int choice_1, choice_2;
-  int data[MAKS] = {3, 10, 4, 6, 8, 9, 7, 2, 1, 5};
-  int bak[MAKS] = {3, 10, 4, 6, 8, 9, 7, 2, 1, 5};
+  Data arr[MAKS], bak[MAKS];
+  int n = input(arr);
+  copy(arr, bak);
   do {
 
-    printf("Kondisi awal array : \n");
-    tampil(data);
+    tampil(arr, n);
     printf("MENU METODE SORTING : \n");
     printf("1. INSERTION \n");
     printf("2. SELECTION \n");
@@ -37,82 +46,101 @@ int main() {
 
     switch (choice_1) {
     case 1:
-      insertion(data, choice_2);
+      insertion(arr, n, choice_2);
       break;
     case 2:
-      selection(data, choice_2);
+      selection(arr, n, choice_2);
       break;
     default:
       printf("INVALID CHOICE METHODE !!\n");
       break;
     }
     printf("Kondisi Akhir \n");
-    tampil(data);
+    tampil(arr, n);
+    copy(bak, arr);
   } while (choice_1 != 3);
 }
 
-void insertion(int arr[], int mode) {
-  int key, kondisi;
-  for (int i = 1; i < 10; i++) {
+void insertion(Data arr[], int n, int mode) {
+  Data key;
+  int j;
+  for (int i = 1; i < n; i++) {
+    key = arr[i]; // Simpan seluruh struct
+    j = i - 1;
 
-    key = arr[i];
-    int j = i - 1;
-
-    if (mode == 2) {
-      while (j >= 0 && arr[j] < key) {
+    if (mode == 2) { // Descending
+      while (j >= 0 && arr[j].no < key.no) {
         arr[j + 1] = arr[j];
         j--;
       }
-    } else {
-      while (j >= 0 && arr[j] > key) {
+    } else { // Ascending
+      while (j >= 0 && arr[j].no > key.no) {
         arr[j + 1] = arr[j];
         j--;
       }
     }
-
     arr[j + 1] = key;
   }
 }
 
-void selection(int arr[], int mode) {
-  int min, temp;
-  for (int i = 0; i < MAKS; i++) {
+void selection(Data arr[], int n, int mode) {
+  int min;
+  for (int i = 0; i < n - 1; i++) {
     min = i;
-    int j = i + 1;
-
-    if (mode == 2) {
-      while (j < MAKS) {
-        if (arr[j] > arr[min])
+    for (int j = i + 1; j < n; j++) { // Gunakan n, bukan MAKS[cite: 1]
+      if (mode == 2) {
+        if (arr[j].no > arr[min].no)
           min = j;
-        j++;
-      }
-    } else {
-      while (j < MAKS) {
-        if (arr[j] < arr[min])
+      } else {
+        if (arr[j].no < arr[min].no)
           min = j;
-        j++;
       }
     }
-
-    tukar(&arr[i], &arr[min]);
-    // temp = arr[i];
-    // arr[i] = arr[min];
-    // arr[min] = temp;
+    tukar(&arr[i], &arr[min]); // Tukar berdasarkan pointer[cite: 1]
   }
 }
 
-void tampil(int arr[]) {
-  int i;
-  printf("[");
-  for (i = 0; i < MAKS - 1; i++)
-    printf("%d,", arr[i]);
-  printf("%d]\n\n", arr[i]);
+void tampil(Data arr[], int n) {
+  printf("%-3s %-20s %5s \n", "No", "Nama", "Nilai");
+  for (int i = 0; i < n; i++)
+    printf("%-3d%-20s%c\n", arr[i].no, arr[i].nama, arr[i].nilai);
+  printf("\n");
 }
 
-void tukar(int a[], int b[]) {
-  int temp;
+void tukar(Data *a, Data *b) {
+  Data temp;
 
   temp = *a;
   *a = *b;
   *b = temp;
+}
+
+int input(Data a[]) {
+  int i = 0;
+  char choice;
+  printf("INPUT USER\n");
+  do {
+
+    printf("Masukkan nomor : ");
+    scanf("%d", &a[i].no);
+    getchar();
+    printf("Masukkan nama : ");
+    fgets(a[i].nama, MAKS, stdin);
+    a[i].nama[strcspn(a[i].nama, "\n")] = '\0';
+    printf("Masukkan nilai : ");
+    scanf("%c", &a[i].nilai);
+    getchar();
+    printf("Lagi (Y/n) : ");
+    scanf("%c", &choice);
+    getchar();
+    if (choice == 'n')
+      break;
+    i++;
+  } while (choice == 'y');
+  return i + 1;
+}
+
+void copy(Data *sc, Data *dest) {
+  for (int i = 0; i < MAKS; i++)
+    dest[i] = sc[i];
 }
