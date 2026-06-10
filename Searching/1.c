cb395ad0917ce0de55550894d60378d1fc81147c
goblock1938 <goblock1938@gmail.com>
2026-06-11 00:23:33 +0700

    mod search

--- Searching/1.c
+++ Searching/1.c
@@ -1,52 +1,94 @@
 #include <stdio.h>
-#define MAKS 5
-void sequent();
-void binary();
-void quick(int arr[], int low, int high, int mode);
+#include <stdlib.h>
+#include <time.h>
+#define MAKS 100000
+
+int sequent(int[], int, int);
+int binary(int[], int, int);
+void generate(int[], int);
+void acak(int[], int);
 void tukar(int *, int *);
-int partition(int Data[], int low, int high, int mode);
 
 int main() {
-  int arr[MAKS] = {5, 2, 3, 7, 4}, search;
+  time_t start, end;
+  int arr[MAKS], n, arr1[MAKS], choice, comp;
+  printf("Berapa data : ");
+  scanf("%d", &n);
 
-  printf("Data yang mau dicari : ");
-  scanf("%d", &search);
+  generate(arr, n);
+  for (int i = 0; i < n; i++)
+    arr1[i] = arr[i];
+  acak(arr1, n);
+
+  while (1) {
+    printf("Searching Metode\n0. Exit\n1. Sequent tanpa Sorting\n2. Sequent "
+           "dengan Sorting\n3. Binary\n");
+    scanf("%d", &choice);
+
+    if (choice == 0) {
+      printf("Selesai\n");
+      break;
+    }
+    start = clock();
+    switch (choice) {
+    case 1:
+      comp = sequent(arr1, n, rand() % n);
+      break;
+    case 2:
+      comp = sequent(arr, n, rand() % n);
+      break;
+    case 3:
+      comp = binary(arr1, n, rand() % n);
+      break;
+    }
+
+    end = clock();
+  };
 }
 
-void sequent() {}
-
-void binary() {}
-
-void quick(int arr[], int low, int high, int mode) {
-  if (low < high) {
-    int pi = partition(arr, low, high, mode);
-
-    quick(arr, low, pi - 1, mode);
-    quick(arr, pi + 1, high, mode);
+int sequent(int arr[], int n, int key) {
+  int comp;
+  for (int i = 0; i < n; i++) {
+    comp++;
+    if (arr[i] == key)
+      return comp;
   }
+  return -1;
 }
 
-int partition(int Data[], int low, int high, int mode) {
-  int pivot = Data[high];
-  int i = (low - 1);
+int binary(int arr[], int n, int key) {
+  int low = 0;
+  int high = n - 1;
+  int comp;
+  while (low <= high) {
+    int mid = low + (high - low) / 2;
 
-  for (int j = low; j < high; j++) {
-    if (mode == 1) {
-      if (Data[j] <= pivot) {
-        i++;
-        tukar(&Data[i], &Data[j]);
-      }
+    comp++;
+    if (arr[mid] == key) {
+      return comp;
+    }
+
+    if (arr[mid] < key) {
+      low = mid + 1;
     } else {
-      if (Data[j] >= pivot) {
-        i++;
-        tukar(&Data[i], &Data[j]);
-      }
+      high = mid - 1;
     }
   }
-  tukar(&Data[i + 1], &Data[high]);
-  return (i + 1);
+  return -1;
 }
 
+void generate(int arr[], int n) {
+  for (int i = 0; i < n; i++)
+    arr[i] = i + 1;
+}
+
+void acak(int arr[], int n) {
+  for (int i = n - 1; i > 0; i--) {
+    int j = rand() % (i + 1);
+
+    tukar(&arr[i], &arr[j]);
+  }
+}
 void tukar(int *a, int *b) {
   int temp = *a;
   *a = *b;
