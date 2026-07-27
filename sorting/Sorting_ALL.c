f946cf3df26d6589a766c9155dadce7ad84bf6b7
goblock1938 <goblock1938@gmail.com>
2026-07-27 12:07:24 +0700

    comment for sorting #2

--- sorting/Sorting_ALL.c
+++ sorting/Sorting_ALL.c
@@ -3,6 +3,9 @@
 #include <time.h>
 #include <unistd.h>
 #define MAKS 100000
+// jikalau bingung dengan visualisasi sorting yang ada di sini, kalian bisa
+// mencoba link berikut : https://visualgo.net/en/sorting
+// namun untuk shell sort tidak tersedia di link tersebut
 
 void insertion(int[], int, int);
 void selection(int[], int, int);
@@ -94,14 +97,17 @@ void tukar(int *a, int *b) {
 }
 
 void insertion(int arr[], int n, int urut) {
-  int key;
+
+  // konsep dari insert sort adalah mengambil data paling pertama yang belum di
+  // urutkan (key) dan memindahkan sampai pada posisi yang tepat
+  int key; // variable untuk menampung key
   for (int i = 1; i < n; i++) {
 
-    key = arr[i];
-    // ambil array pertama yang belum di sorting
+    key = arr[i]; // pengambilan data pertama
     int j = i - 1;
 
     if (urut == 2) {
+      // Ascending
       while (j >= 0 && arr[j] < key) {
         arr[j + 1] = arr[j];
         j--;
@@ -118,6 +124,9 @@ void insertion(int arr[], int n, int urut) {
 }
 
 void selection(int arr[], int n, int urut) {
+
+  // konsep dari select sort adalah mencari data
+  // paling kecil dari data yang belum di urutkan
   int min;
   for (int i = 0; i < n; i++) {
     min = i;
@@ -142,7 +151,15 @@ void selection(int arr[], int n, int urut) {
 }
 
 void bubble(int arr[], int n, int urut) {
+
+  // konsep dari bubble sort adalah menukar data
+  // sebelah kiri dan kanan jika urutannya salah
+
   int isSwap = 1, i = 0;
+  // isSwap variable untuk statement apakah dia masih menukar atau tidak
+  // jikalau isSwap bernilai 0 atau false
+  // maka bubble sort akan langsung berhenti
+
   while (i < n && isSwap) {
     isSwap = 0;
     for (int j = 0; j < n - i - 1; j++) {
@@ -165,6 +182,10 @@ void bubble(int arr[], int n, int urut) {
 }
 
 void shell(int arr[], int n, int urut) {
+  // shell sort memiliki konsep gap yang dimana dia akan
+  // meng-compare suatu data dengan data yang
+  // memiliki jarak(gap) tertentu
+
   for (int gap = n / 2; gap > 0; gap /= 2) {
     for (int i = gap; i < n; i++) {
       int temp = arr[i];
@@ -186,6 +207,9 @@ void shell(int arr[], int n, int urut) {
 
 // merge function pertama untuk melakukan partisi
 void merge(int arr[], int res[], int low, int high, int urut) {
+
+  // partisi merge dan partisi quick berbeda metode.
+  // untuk merge mereka partisi menjadi 2 lalu di gabung kembali
   if (low < high) {
     int medium = (low + high) / 2;
     merge(arr, res, low, medium, urut);
@@ -196,13 +220,17 @@ void merge(int arr[], int res[], int low, int high, int urut) {
 
 // merge function kedua untuk memproses tiap partisinya
 void merging(int arr[], int res[], int low, int medium, int high, int urut) {
-  int L1 = low;
-  int R1 = medium;
-  int L2 = medium + 1;
-  int R2 = high;
+  int L1 = low;        // low partisi kiri
+  int R1 = medium;     // high partisi kiri
+  int L2 = medium + 1; // low partisi kanan
+  int R2 = high;       // high partisi kanan
   int i = low;
 
   while (L1 <= R1 && L2 <= R2) {
+
+    // compare data dimulai dari low kedua sisi
+    // jika low kiri lebih kecil dari low kanan
+    // maka low kiri di dahulukan, begitu juga sebaliknya
     if (arr[L1] <= arr[L2]) {
       res[i] = arr[L1];
       L1++;
@@ -212,6 +240,8 @@ void merging(int arr[], int res[], int low, int medium, int high, int urut) {
     }
     i++;
   }
+
+  // loop di bawah akan ter-eksekusi jika sala satu dari partisi sudah habis
   while (L1 <= R1) {
     res[i] = arr[L1];
     L1++;
@@ -226,6 +256,7 @@ void merging(int arr[], int res[], int low, int medium, int high, int urut) {
 
   int j = low;
 
+  // hasil dari array baru akan kembali ke array lama
   while (j <= high) {
     arr[j] = res[j];
     j++;
@@ -234,6 +265,8 @@ void merging(int arr[], int res[], int low, int medium, int high, int urut) {
 
 // function quick sort pertama untuk partisi
 void quick(int arr[], int low, int high, int urut) {
+
+  // untuk partisi quick menggunakan pivot sebagai acuan data
   if (low < high) {
     int pi = partition(arr, low, high);
 
@@ -244,15 +277,19 @@ void quick(int arr[], int low, int high, int urut) {
 
 // partition for quick sort
 int partition(int Data[], int low, int high) {
-  int pivot = Data[high];
-  int i = (low - 1);
+  int pivot = Data[high]; // sebuah titik acuan untuk partisi
+  int i = low - 1;
 
   for (int j = low; j < high; j++) {
+
+    // data <= pivot untuk mengkalsifikasi siapa saja yang
+    // di kanan (lebih besar) dan yang di kiri (lebih kecil)
     if (Data[j] <= pivot) {
       i++;
       tukar(&Data[i], &Data[j]);
     }
   }
+
   tukar(&Data[i + 1], &Data[high]);
   return (i + 1);
 }
